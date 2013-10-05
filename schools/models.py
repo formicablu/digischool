8ff3f4b305da41f1193e72423955db673f24c925
Marco Montanari <sirmmo@gmail.com>
2013-10-05 16:06:11 +0200

    update

--- schools/models.py
+++ schools/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Year(models.Model):
+	name = models.CharField(max_length=4)
+	def __str__(self):
+		return self.name
+
+class School(models.Model):
+	name = models.CharField(max_length=255)
+
+class MetricType(models.Model):
+	name = models.CharField(max_length=500)
+
+class Metric
+	school = models.ForeignKey(School)
+	metric_value = models.IntegerField()
+	metric_type = models.ForeignKey(MetricType)
+		
+
